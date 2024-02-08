d975b148fcf39e6787625be91668b9e0177d8f5d
sceiiya <meetscheidj@gmail.com>
2024-02-09 01:53:37 +0800

    test cards

--- src/components/HomepageSection.tsx
+++ src/components/HomepageSection.tsx
@@ -1,21 +1,59 @@
 import { IfHomepageSection } from "../types/interfaces"
 import Cards from './Cards';
+import goimage from "../assets/images/Genmaicha.jpg"
+import Buttonz from "./Buttonz";
 
-const HomepageSection:React.FC<IfHomepageSection> = ({className, cards}) => {
-    const [stringContent, numberContent, buttonElement] = cards
-
+const HomepageSection:React.FC<IfHomepageSection> = ({className}) => {
+    // const [stringContent, numberContent, buttonElement] = cards
     return (
         <>
-            <div>HomepageSection
-                <div className={className}></div>
-                <div>{stringContent}</div>
-                <div>{numberContent}</div>
-                <div>{buttonElement}</div>
-                <Cards 
-                    className="p-3"
-                    heading="head"
-                    subheading="subhead"  
-                />
+            <div className={className}>
+                <div className="cards-container flex">
+                    <Cards
+                        className="p-3"
+                        heading="Wakoucha"
+                        subheading="Smooth Japanese black tea with subtle sweetness and delicate flavor notes."
+                        imgage={goimage}
+                        currency="$"
+                        price={150}
+                        button1={<Buttonz text="Buy Now" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent hover:text-white hover:bg-[#539941] font-bold shadow-none hover:shadow-md hover:shadow-black/30" />}
+                        button2={<Buttonz text="Discover" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent  font-bold hover:shadow-md hover:shadow-black/30" />}
+                        stock={"60"}
+                    />
+                    <Cards
+                        className="p-3"
+                        heading="Wakoucha"
+                        subheading="Smooth Japanese black tea with subtle sweetness and delicate flavor notes."
+                        imgage={goimage}
+                        currency="$"
+                        price={30}
+                        button1={<Buttonz text="Buy Now" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent hover:text-white hover:bg-[#539941] font-bold shadow-none hover:shadow-md hover:shadow-black/30" />}
+                        button2={<Buttonz text="Discover" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent  font-bold hover:shadow-md hover:shadow-black/30" />}
+                        stock={"0"}
+                    />
+                    <Cards
+                        className="p-3"
+                        heading="Wakoucha"
+                        subheading="Smooth Japanese black tea with subtle sweetness and delicate flavor notes."
+                        imgage={goimage}
+                        currency="$"
+                        price={69}
+                        button1={<Buttonz text="Buy Now" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent hover:text-white hover:bg-[#539941] font-bold shadow-none hover:shadow-md hover:shadow-black/30" />}
+                        button2={<Buttonz text="Discover" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent  font-bold hover:shadow-md hover:shadow-black/30" />}
+                        stock={"2"}
+                    />
+                    <Cards
+                        className="p-3"
+                        heading="Wakoucha"
+                        subheading="Smooth Japanese black tea with subtle sweetness and delicate flavor notes."
+                        imgage={goimage}
+                        currency="$"
+                        price={340}
+                        button1={<Buttonz text="Buy Now" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent hover:text-white hover:bg-[#539941] font-bold shadow-none hover:shadow-md hover:shadow-black/30" />}
+                        button2={<Buttonz text="Discover" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent  font-bold hover:shadow-md hover:shadow-black/30" />}
+                        stock={"40"}
+                    />
+                </div>
             </div>
         </>
     )
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -7,8 +7,8 @@ function Home() {
   return (
     <>
         <Helmet>
-            <title>WashiTea | Home</title>
-            <meta name="description" content="Homepage of WashiTea"/>
+            <title>WashiCha | Home</title>
+            <meta name="description" content="Homepage of WashiCha"/>
             <link rel="canonical" href="/home" data-rh="true" />
         </Helmet>
         <div className="flex justify-center h-screen items-center px-[12%] -mt-10">
@@ -20,14 +20,14 @@ function Home() {
                     <h1 className="text-slate-600 font-extrabold text-4xl tracking-wide uppercase">Elevate Your Tea Experience with WashiCha's Exquisite Collection</h1>
                     <h2 className="text-slate-600 text-xl mt-5">Indulge in the Rich Heritage and Health Benefits of Japanese Teas. Savor the Pure Delight of Handpicked Quality Tea Leaves.</h2>
                     <div className="mt-6">
-                        <a href="#" className="p-4 rounded-lg bg-[#539941] text-white font-bold shadow-none transition-shadow duration-300 ease-in-out hover:shadow-lg hover:shadow-black/30 ">Join WashiTea</a>
+                        <a href="#" className="p-4 rounded-lg bg-[#539941] text-white font-bold shadow-none transition-shadow duration-300 ease-in-out hover:shadow-lg hover:shadow-black/30 ">Join WashiCha</a>
                         <a href="#" className="p-4 rounded-lg text-[#539941] bg-[#87f86794] hover:text-white transition-all hover:bg-[#539941] font-bold shadow-none duration-300 ease-in-out hover:shadow-lg hover:shadow-black/30 ms-2 ">Discover Teas</a>
                     </div>
                 </div>
             </div>
         </div>
         <div className="flex justify-center h-screen items-center px-[12%]">1 tea</div>
-        <HomepageSection className="flex justify-center h-screen items-center px-[12%]" cards={boom}></HomepageSection>
+        <HomepageSection className="flex  justify-center h-screen items-center px-[12%]" cards={boom}></HomepageSection>
         <div className="flex justify-center h-screen items-center px-[12%]">A blog</div>
         <div className="flex justify-center h-screen items-center px-[12%]">3 Teas</div>
         <div className="flex justify-center h-screen items-center px-[12%]">HealthTea</div>

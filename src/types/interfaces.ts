4fa338a4c1fd71444dbb82157f728bb84bbc6f0a
sceiiya <meetscheidj@gmail.com>
2024-02-08 16:28:31 +0800

    add section and card interfaces

---
+++ src/types/interfaces.ts
@@ -0,0 +1,16 @@
+export interface IfHomepageSection {
+    className: string;
+    cards: (number | string | JSX.Element)[];
+}
+
+export interface IfCards{
+    className: string;
+    heading: string;
+    subheading: string;
+    button1?: JSX.Element;
+    button2?: JSX.Element;
+    price?: number;
+    currency?: string;
+    imgage?: any;
+    stock?: number;
+}

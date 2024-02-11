74536e1758b792dda57cb78fa12209b6ec92efd6
sceiiya <meetscheidj@gmail.com>
2024-02-11 22:54:00 +0800

    add note

--- src/components/HomepageSection.tsx
+++ src/components/HomepageSection.tsx
@@ -5,6 +5,7 @@ import Buttonz from "./Buttonz";
 
 const HomepageSection:React.FC<IfHomepageSection> = ({className}) => {
     // const [stringContent, numberContent, buttonElement] = cards
+    // const [className, heading, subheading, imgage, currency, price, button1, button2, stock] = cards 
     return (
         <>
             <div className={className}>
@@ -19,39 +20,6 @@ const HomepageSection:React.FC<IfHomepageSection> = ({className}) => {
                         button1={<Buttonz text="Buy Now" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent hover:text-white hover:bg-[#539941] font-bold shadow-none hover:shadow-md hover:shadow-black/30" />}
                         button2={<Buttonz text="Discover" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent  font-bold hover:shadow-md hover:shadow-black/30" />}
                         stock={"60"}
-                    />
-                    <Cards
-                        className="p-3"
-                        heading="Wakoucha"
-                        subheading="Smooth Japanese black tea with subtle sweetness and delicate flavor notes."
-                        imgage={goimage}
-                        currency="$"
-                        price={30}
-                        button1={<Buttonz text="Buy Now" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent hover:text-white hover:bg-[#539941] font-bold shadow-none hover:shadow-md hover:shadow-black/30" />}
-                        button2={<Buttonz text="Discover" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent  font-bold hover:shadow-md hover:shadow-black/30" />}
-                        stock={"0"}
-                    />
-                    <Cards
-                        className="p-3"
-                        heading="Wakoucha"
-                        subheading="Smooth Japanese black tea with subtle sweetness and delicate flavor notes."
-                        imgage={goimage}
-                        currency="$"
-                        price={69}
-                        button1={<Buttonz text="Buy Now" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent hover:text-white hover:bg-[#539941] font-bold shadow-none hover:shadow-md hover:shadow-black/30" />}
-                        button2={<Buttonz text="Discover" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent  font-bold hover:shadow-md hover:shadow-black/30" />}
-                        stock={"2"}
-                    />
-                    <Cards
-                        className="p-3"
-                        heading="Wakoucha"
-                        subheading="Smooth Japanese black tea with subtle sweetness and delicate flavor notes."
-                        imgage={goimage}
-                        currency="$"
-                        price={340}
-                        button1={<Buttonz text="Buy Now" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent hover:text-white hover:bg-[#539941] font-bold shadow-none hover:shadow-md hover:shadow-black/30" />}
-                        button2={<Buttonz text="Discover" className="text-[#539941] border-[#539941] border-solid border hover:border-transparent  font-bold hover:shadow-md hover:shadow-black/30" />}
-                        stock={"40"}
                     />
                 </div>
             </div>

12fd22e2006b06651953acf77d25a2b65faf9b1a
sceiiya <meetscheidj@gmail.com>
2024-02-11 22:53:22 +0800

    add notes for the next dev session

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -11,10 +11,10 @@ function Home() {
             <meta name="description" content="Homepage of WashiCha"/>
             <link rel="canonical" href="/home" data-rh="true" />
         </Helmet>
-        <div className="flex justify-center h-screen items-center px-[12%] -mt-10">
-            <div className="flex justify-center items-start">
-                <div className="p-3">
-                    <img className="h-auto max-w-lg rounded-lg shadow-none transition-shadow duration-300 ease-in-out hover:shadow-lg hover:shadow-black/30" src={FukamushiImg} alt="Fukamushi Tea" />
+        <div className="flex justify-center h-screen items-center px-[12%] max-md:px-1 -mt-10 max-sm:mt-10 ">
+            <div className="flex justify-center items-start max-sm:flex-col ">
+                <div className="p-3 max-md:w-2/5 max-sm:w-full">
+                    <img className="h-auto w-full rounded-lg shadow-none transition-shadow duration-300 ease-in-out hover:shadow-lg hover:shadow-black/30" src={FukamushiImg} alt="Fukamushi Tea" />
                 </div>
                 <div className="p-3 mt-20">
                     <h1 className="text-slate-600 font-extrabold text-4xl tracking-wide uppercase">Elevate Your Tea Experience with WashiCha's Exquisite Collection</h1>
@@ -26,12 +26,27 @@ function Home() {
                 </div>
             </div>
         </div>
-        <div className="flex justify-center h-screen items-center px-[12%]">1 tea</div>
-        <HomepageSection className="flex  justify-center h-screen items-center px-[12%]" cards={boom}></HomepageSection>
-        <div className="flex justify-center h-screen items-center px-[12%]">A blog</div>
-        <div className="flex justify-center h-screen items-center px-[12%]">3 Teas</div>
-        <div className="flex justify-center h-screen items-center px-[12%]">HealthTea</div>
-        <div className="flex justify-center h-screen items-center px-[12%]">Delivery service</div> 
+        <div className="flex justify-center h-screen items-center px-[12%]">
+            1 tea
+            Only 1 tea should be displayed here.. like a card but the pictur should be maybe in the right?
+        </div>
+        <div className="flex justify-center h-screen items-center px-[12%]">
+            A blog
+            Get at 'article' like  for this section
+        </div>
+        <HomepageSection
+            className="flex justify-center h-screen items-center px-[12%]"
+            cards={boom}
+        >
+        </HomepageSection>
+        <div className="flex justify-center h-screen items-center px-[12%]">
+            HealthTea
+            Should have 3 Cards here each benefit for card as a tea consumer
+        </div>
+        <div className="flex justify-center h-screen items-center px-[12%]">
+            Delivery service
+            Should have cards also here or just a picture and an icon about the delivery services or the benefits of using the platform
+        </div> 
     </>
   )
 }

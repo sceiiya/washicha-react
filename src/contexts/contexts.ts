dd9563738c4a7431fb83131cbe05b07144d36d8f
sceiiya <meetscheidj@gmail.com>
2024-02-16 01:33:52 +0800

    INIT SAMPLE DATA

---
+++ src/contexts/contexts.ts
@@ -0,0 +1,44 @@
+import goimage from "../assets/images/Genmaicha.jpg"
+
+export const teas = [
+    {
+        heading: "Wakoucha",
+        subheading: "Smooth Japanese black tea with subtle sweetness and delicate flavor notes.",
+        button1: "Buy Now",
+        button2: "Discover",
+        price: 69,
+        currency: "$",
+        imgage: {goimage},
+        stock: 56
+    },
+    {
+        heading: "Wakoucha",
+        subheading: "Smooth Japanese black tea with subtle sweetness and delicate flavor notes.",
+        button1: "Buy Now",
+        button2: "Discover",
+        price: 150,
+        currency: "$",
+        imgage: {goimage},
+        stock: 23
+    },
+    {
+        heading: "Wakoucha",
+        subheading: "Smooth Japanese black tea with subtle sweetness and delicate flavor notes.",
+        button1: "Buy Now",
+        button2: "Discover",
+        price: 96,
+        currency: "$",
+        imgage: {goimage},
+        stock: 156
+    },
+    {
+        heading: "Wakoucha",
+        subheading: "Smooth Japanese black tea with subtle sweetness and delicate flavor notes.",
+        button1: "Buy Now",
+        button2: "Discover",
+        price: 1,
+        currency: "$",
+        imgage: {goimage},
+        stock: "0"
+    },
+]
